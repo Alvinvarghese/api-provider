aec12004f754d13cddcfce0641a018d2d5858c61
Alvin Varghese <alvin19official@gmail.com>
2024-04-28 22:45:15 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+
+const path = require("path");
+const dotenv = require("dotenv");
+const root_dir = __dirname.split("src")[0];
+dotenv.config({ path: path.join(root_dir, `.env`) });
+const cors = require("cors");
+
+const morgan = require("morgan");
+app.use(morgan("tiny"));
+
+const corsOptions = {
+    origin: `${process.env.APP_URL}`,
+    credentials: true,
+};
+app.use(cors(corsOptions));
+app.use(express.json());
+
+app.get("/api", (req, res) => {
+  res.status(200).json({ link: "https://localhost:3000/" });
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`App listening at port ${port}`);
+});
